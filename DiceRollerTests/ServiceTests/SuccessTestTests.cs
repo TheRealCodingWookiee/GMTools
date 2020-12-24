9fb2a03c32949f29d268a453647ef4511d941e20
Kostas <weedywookie@gmail.com>
2020-12-24 11:16:23 +0100

    test for threshold pass method

---
+++ DiceRollerTests/ServiceTests/SuccessTestTests.cs
@@ -0,0 +1,70 @@
+﻿using DiceRoller.Models;
+using DiceRoller.Services.Tests;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DiceRollerTests.ServiceTests
+{
+    public class SuccessTestTests
+    {
+
+        public DicePool dicePool;
+        public SuccessTest successTest;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            dicePool = new DicePool(6);
+            successTest = new SuccessTest(dicePool);
+        }
+
+        [Test]
+        public void PassThresholdTest()
+        {
+            int threshold = 3;
+            successTest.Threshold = threshold;
+            List<int> results = new List<int> { 2, 5, 6, 5, 5 };
+            List<Dice> dicePool = CreateDicePool(results);
+
+            bool passed = successTest.IsThresholdPassed(dicePool);
+
+
+            Assert.IsTrue(passed);
+        }
+
+        [Test]
+        public void FailThresholdTest()
+        {
+            int threshold = 3;
+            successTest.Threshold = threshold;
+            List<int> results = new List<int> { 2, 5, 5, 2, 3, 4 };
+            List<Dice> dicePool = CreateDicePool(results);
+
+            bool passed = successTest.IsThresholdPassed(dicePool);
+
+
+            Assert.IsFalse(passed);
+        }
+
+
+        private Dice CreateTestDice(int diceResult)
+        {
+            Dice dice = new Dice();
+            dice.DiceResult = diceResult;
+            return dice;
+        }
+
+        private List<Dice> CreateDicePool(List<int> results)
+        {
+            List<Dice> dicePool = new List<Dice>();
+
+            foreach (int result in results)
+            {
+                dicePool.Add(CreateTestDice(result));
+            }
+
+            return dicePool;
+        }
+
+    }
+}
