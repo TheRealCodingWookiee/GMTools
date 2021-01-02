75ef881f6555b4ed9a0928cf9e3bd22837a46d87
Kostas <weedywookie@gmail.com>
2021-01-03 00:02:50 +0100

    tests added

--- DiceRollerTests/ServiceTests/SuccessTestTests.cs
+++ DiceRollerTests/ServiceTests/SuccessTestTests.cs
@@ -15,18 +15,17 @@ namespace DiceRollerTests.ServiceTests
         public void Setup()
         {
             dicePool = new DicePool(6);
-            successTest = new SuccessTest(dicePool);
+            successTest = new SuccessTest();
         }
 
         [Test]
         public void PassThresholdTest()
         {
             int threshold = 3;
-            successTest.Threshold = threshold;
             List<int> results = new List<int> { 2, 5, 6, 5, 5 };
             List<Dice> dicePool = CreateDicePool(results);
 
-            bool passed = successTest.IsThresholdPassed(dicePool);
+            bool passed = successTest.IsThresholdPassed(dicePool, threshold);
 
 
             Assert.IsTrue(passed);
@@ -36,11 +35,10 @@ namespace DiceRollerTests.ServiceTests
         public void FailThresholdTest()
         {
             int threshold = 3;
-            successTest.Threshold = threshold;
             List<int> results = new List<int> { 2, 5, 5, 2, 3, 4 };
             List<Dice> dicePool = CreateDicePool(results);
 
-            bool passed = successTest.IsThresholdPassed(dicePool);
+            bool passed = successTest.IsThresholdPassed(dicePool, threshold);
 
 
             Assert.IsFalse(passed);
@@ -49,10 +47,132 @@ namespace DiceRollerTests.ServiceTests
         [Test]
         public void GlitchOnRolledDicePoolTest()
 		{
-            
-            successTest.IsGlitch();
-            Assert.Pass();
+            List<int> results = new List<int> { 1, 1, 1, 6 };
+            List<Dice> dicePool = CreateDicePool(results);
+            DicePool pool = new DicePool(4);
+            pool.Pool = dicePool;
 
+            bool glitched = successTest.IsGlitch(pool);
+            Assert.IsTrue(glitched);
+
+        }
+
+        [Test]
+        public void NoGlitchOnRolledDicePoolTest()
+        {
+            List<int> results = new List<int> { 1, 1, 3, 2, 4, 6 };
+            List<Dice> dicePool = CreateDicePool(results);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dicePool;
+
+            bool glitched = successTest.IsGlitch(pool);
+            Assert.IsFalse(glitched);
+
+        }
+
+        [Test]
+        public void CriticalGlitchOnRolledDicePoolTest()
+        {
+            List<int> results = new List<int> { 1, 1, 1, 1, 4, 3 };
+            List<Dice> dicePool = CreateDicePool(results);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dicePool;
+
+            bool criticalGlitch = successTest.IsCriticalGlitch(pool);
+
+            Assert.IsTrue(criticalGlitch);
+        }
+
+        [Test]
+        public void NoCriticalGlitchOnRolledDicePoolTest()
+        {
+            List<int> results = new List<int> { 1, 1, 1, 1, 4, 6 };
+            List<Dice> dicePool = CreateDicePool(results);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dicePool;
+
+            bool criticalGlitch = successTest.IsCriticalGlitch(pool);
+
+            Assert.IsFalse(criticalGlitch);
+        }
+
+        [Test]
+        public void GetSuccesTestResultCriticalGlitch()
+        {
+            List<int> criticalGlitch = new List<int> { 1, 1, 1, 1, 4, 3 };
+            List<Dice> dices = CreateDicePool(criticalGlitch);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dices;
+
+
+            string expected = "Critical Glitch!";
+
+            string actual = successTest.GetSuccessTestResult(pool, 3);
+
+            Assert.IsTrue(expected == actual);
+        }
+
+        [Test]
+        public void GetSuccesTestResultGlitch()
+        {
+            List<int> glitch = new List<int> { 1, 1, 1, 1, 6, 6 };
+            List<Dice> dices = CreateDicePool(glitch);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dices;
+
+
+            string expected = "Not passed and Glitch!";
+
+            string actual = successTest.GetSuccessTestResult(pool, 3);
+
+            Assert.IsTrue(expected == actual);
+        }
+
+        [Test]
+        public void GetSuccesTestResultPassedAndGlitch()
+        {
+            List<int> glitch = new List<int> { 1, 1, 1, 6, 6, 6 };
+            List<Dice> dices = CreateDicePool(glitch);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dices;
+
+
+            string expected = "Passed with Glitch!";
+
+            string actual = successTest.GetSuccessTestResult(pool, 3);
+
+            Assert.IsTrue(expected == actual);
+        }
+
+        [Test]
+        public void GetSuccesTestResultPassed()
+        {
+            List<int> glitch = new List<int> { 1, 1, 4, 6, 6, 6 };
+            List<Dice> dices = CreateDicePool(glitch);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dices;
+
+
+            string expected = "Passed!";
+
+            string actual = successTest.GetSuccessTestResult(pool, 3);
+
+            Assert.IsTrue(expected == actual);
+        }
+        [Test]
+        public void GetSuccesTestResultNotPassed()
+        {
+            List<int> glitch = new List<int> { 1, 1, 4, 4, 2, 6 };
+            List<Dice> dices = CreateDicePool(glitch);
+            DicePool pool = new DicePool(6);
+            pool.Pool = dices;
+
+
+            string expected = "Not passed!";
+
+            string actual = successTest.GetSuccessTestResult(pool, 3);
+
+            Assert.IsTrue(expected == actual);
         }
 
 
