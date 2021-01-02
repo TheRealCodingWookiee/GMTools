f8bf223685f02d17872c1a012549217956fbbbfa
Kostas <weedywookie@gmail.com>
2021-01-03 00:02:27 +0100

    method for getting result, refactored parameters

--- DiceRoller/Services/Tests/SuccessTest.cs
+++ DiceRoller/Services/Tests/SuccessTest.cs
@@ -12,49 +12,81 @@ namespace DiceRoller.Services.Tests
     public class SuccessTest : ITest
     {
 
-        private int _threshold;
-        private bool _isTestSuccessfull;
-        private DicePool _dicePool;
-
-        public SuccessTest(DicePool dicePool)
+        public SuccessTest()
         {
-            _dicePool = dicePool;
         }
 
-        public int Threshold 
-        { 
-            get { return _threshold; } 
-            set { _threshold = value; } 
+        public string GetSuccessTestResult(DicePool dicePool, int threshold)
+        {
+            string thresholdResult = "";
+
+            bool criticalGlitch = IsCriticalGlitch(dicePool);
+            bool isGlitch = IsGlitch(dicePool);
+            bool thresholdPassed = IsThresholdPassed(dicePool.Pool, threshold);
+
+            if (criticalGlitch)
+            {
+                thresholdResult = "Critical Glitch!";
+            }
+            else if(isGlitch && !thresholdPassed)
+            {
+                thresholdResult = "Not passed and Glitch!";
+            }
+            else if (thresholdPassed && isGlitch)
+            {
+                thresholdResult = "Passed with Glitch!";
+            }
+            else if (thresholdPassed)
+            {
+                thresholdResult = "Passed!";
+            }
+            else if(!thresholdPassed)
+            {
+                thresholdResult = "Not passed!";
+            }
+
+            return thresholdResult;
         }
 
-
-        public bool IsThresholdPassed(List<Dice> dicePool)
+        public bool IsThresholdPassed(List<Dice> dicePool, int threshold)
         {
             bool passed = false;
-
-            if (_dicePool.GetHitsInRolledDicePool(dicePool) >= _threshold)
+            int hitsCount = dicePool.Where(d => d.SuccessRoll == true).ToList().Count();
+            if (hitsCount >= threshold)
             {
                 passed = true;
             }
 
             return passed;           
         }
-
-        public bool IsCriticalGlitch()
+        //Critical Glitch when Glitch + no hits on the dice pool
+        public bool IsCriticalGlitch(DicePool dicePool)
         {
-            throw new NotImplementedException();
+            bool isCriticalGlitch = false;
+            bool isGlitch = IsGlitch(dicePool);
+
+            int hits = dicePool.Pool.Where(d => d.SuccessRoll == true).ToList().Count;
+
+            isCriticalGlitch = (isGlitch && hits == 0) ? true : false; 
+
+            return isCriticalGlitch;
+        }
+        //Glitch when half or more of the dice pool are 1s
+        public bool IsGlitch(DicePool dicePool)
+        {
+            bool isGlitch = false;
+            List<Dice> glitchPool = dicePool.Pool.Where(d => d.DiceResult == 1).ToList();
+            int dividedDicePool = DivideDicePoolByTwoAndRoundUp(dicePool);
+
+            isGlitch = glitchPool.Count >= dividedDicePool ? true : false;
+
+            return isGlitch;
         }
 
-        public bool IsGlitch()
-        {
-            int x = DivideDicePoolByTwoAndRoundUp();
-            return true;
-        }
-
-        private int DivideDicePoolByTwoAndRoundUp()
+        private int DivideDicePoolByTwoAndRoundUp(DicePool dicePool)
 		{
             int dividedDicePool = 0;
-            dividedDicePool = (int) Math.Round((decimal)(_dicePool.DiceCount/2), MidpointRounding.AwayFromZero);
+            dividedDicePool = (int) Math.Round((decimal)(dicePool.DiceCount/2), MidpointRounding.AwayFromZero);
             return dividedDicePool;
 		}
     }
